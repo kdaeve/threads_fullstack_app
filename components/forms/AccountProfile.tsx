64a1bd967cf3cec1298d3c248e5298ba52d4ce15
DeltaXxx <triple_h@outlook.com>
2023-10-07 14:10:46 -0700

    Finish part of back-end section

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,7 +19,9 @@ import * as z from "zod";
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
-import { useUploadThing } from '@/lib/uploadthing'
+import { useUploadThing } from '@/lib/uploadthing';
+import { usePathname, useRouter } from 'next/navigation';
+import { updateUser } from '@/lib/actions/user.actions';
 
 interface Props {
     user: {
@@ -34,8 +36,12 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
-const [files, setFiles] = useState<File[]>([]);
-const { startUpload } = useUploadThing("media");
+  const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
+
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -80,7 +86,20 @@ const { startUpload } = useUploadThing("media");
             }
         }
 
-        // TODO: Update user profile
+        await updateUser({
+          userId: user.id,
+          username: values.username,
+          name: values.name,
+          bio: values.bio,
+          image: values.profile_photo,
+          path: pathname,
+        });
+        
+        if (pathname === '/profile/edit') {
+          router.back();
+        } else {
+          router.push('');
+        }
     }
 
     return (
@@ -124,6 +143,7 @@ const { startUpload } = useUploadThing("media");
                     onChange={(e) => handleImage(e, field.onChange)}
                   />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -143,6 +163,7 @@ const { startUpload } = useUploadThing("media");
                     {...field}
                   />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -162,6 +183,7 @@ const { startUpload } = useUploadThing("media");
                     {...field}
                   />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -181,6 +203,7 @@ const { startUpload } = useUploadThing("media");
                     {...field}
                   />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
